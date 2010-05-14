4f8724bfd72517b31494b660d13c25dd5211ca28
Alcides Fonseca <me@alcidesfonseca.com>
2010-05-14 18:07:36 +0000

    Merge branch 'master' of github.com:alcides/Aeminium
    Conflicts:
    	AeminiumRuntime/src/aeminiumruntime/examples/Main.java
    	AeminiumRuntime/src/aeminiumruntime/linear/LinearRuntime.java
    	aeminium-runtime/build.xml
    	aeminium-runtime/nbproject/project.properties
    	aeminium-runtime/src/aeminiumruntime/Main.java
    	aeminium-runtime/src/aeminiumruntime/TaskGraph.java
    	aeminium-runtime/src/aeminiumruntime/examples/Main.java
    	aeminium-runtime/src/aeminiumruntime/linear/LinearTask.java

--- aeminium-runtime/src/aeminiumruntime/TaskGraph.java
+++
@@ -1,9 +0,0 @@
-package aeminiumruntime;
-
-import java.util.Collection;
-import java.util.Iterator;
-
-public interface TaskGraph extends Iterator {
-    public boolean add(RuntimeTask task, Collection<RuntimeTask> deps);
-    public boolean isDone();
-}
