57735c4c49ec2a2dcc8c963dac7f0132b368abcc
Alcides Fonseca <me@alcidesfonseca.com>
2010-06-19 23:07:05 +0000

    Statistics done.

---
+++ aeminium-runtime/src/aeminiumruntime/StatisticTask.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package aeminiumruntime;
+
+import aeminium.statistics.Statistics;
+
+public abstract class StatisticTask {
+
+    public Statistics statistics;
+
+    public abstract Statistics getStatistics();
+
+}
